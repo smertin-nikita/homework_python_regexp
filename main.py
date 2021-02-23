24531fea471b5aa5d32f241f697296c2acaf3f0b
Smertin Nikita <smertin-nikita@mail.ru>
2021-02-23 19:46:04 +0500

    Added encoding

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import csv
 
 
 def read_csv():
-    with open("phonebook_raw.csv") as f:
+    with open("phonebook_raw.csv", encoding='utf-8') as f:
         rows = csv.reader(f, delimiter=",")
         contacts_list = list(rows)
     return contacts_list
